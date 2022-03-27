10182439f88969f379cdddac7b22f0b333616a5a
savawedev <andrei.sava.dev@gmail.com>
2022-03-27 13:56:32 +0300

    Added game function to play a 5 round game

--- script.js
+++ script.js
@@ -60,6 +60,31 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  playerScore = 0;
-  playerScore = 0;
+  let playerScore = 0;
+  let computerScore = 0;
+
+  console.log(`Game Start!`);
+  console.log(`Player Score: ${playerScore}`);
+  console.log(`Computer Score: ${computerScore}`);
+
+  for (let i = 1; i <= 5; i++) {
+    const playerSelection = window.prompt();
+
+    result = playRound(playerSelection, computerPlay());
+
+    if (result === "draw") {
+      playerScore++;
+      computerScore++;
+    } else if (result === "playerWins") {
+      playerScore++;
+    } else if (result === "computerWins") {
+      computerScore++;
+    } else {
+      console.log(`Oops, something went wrong!`);
+      return;
+    }
+
+    console.log(`Player Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+  }
 }
