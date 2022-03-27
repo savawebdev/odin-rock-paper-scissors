008d1f8bfafd46421ad3c1a0f1d050904c9a91ac
savawedev <andrei.sava.dev@gmail.com>
2022-03-27 13:37:11 +0300

    Added function playRound

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ function computerPlay() {
       break;
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return `It's a draw! You both selected ${playerSelection}!`;
+  }
+
+  if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+  }
+
+  if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+  }
+
+  if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+  }
+}
